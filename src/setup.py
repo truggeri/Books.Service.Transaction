2d469b96e0072f8eecb6d8abe4f076a38f0a31d7
Thomas Ruggeri <truggeri@gmail.com>
2018-05-20 20:01:24 -0400

    Minor updates to setup values.

--- src/setup.py
+++ src/setup.py
@@ -3,9 +3,13 @@ from setuptools import setup
 setup(
     name='BooksServiceTransaction',
     packages=['BooksServiceTransaction'],
+    version="0.0.1",
     include_package_data=True,
     install_requires=[
         'flask',
         'flask-classful'
-    ]
+    ],
+
+    author="Thomas Ruggeri",
+    author_email="thomaspublicext@gmail.com",
 )
