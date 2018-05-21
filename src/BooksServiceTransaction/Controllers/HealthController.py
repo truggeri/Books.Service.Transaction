13ce6218216e3906deb22ac45543f75d08490b3f
Thomas Ruggeri <truggeri@gmail.com>
2018-05-20 20:15:01 -0400

    Updating health sub-routes.

--- src/BooksServiceTransaction/Controllers/HealthController.py
+++ src/BooksServiceTransaction/Controllers/HealthController.py
@@ -4,11 +4,11 @@ from flask_classful import FlaskView, route
 class HealthController(FlaskView):
     """A class of health related endpoints"""
 
-    @route("/ok")
+    @route("ok")
     def Ok(self):
-        return ("ok\n", 200)
+        return ("ok", 200)
 
-    @route("/health")
+    @route("health")
     def Health(self):
         result = {'healthy' : True}
         return (jsonify(result), 200)
