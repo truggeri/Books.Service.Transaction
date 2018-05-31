3be8a790fd08413ed03c5c74643d3d2e89ee472f
Thomas Ruggeri <truggeri@gmail.com>
2018-05-30 20:33:42 -0400

    Adding quick clean script.

---
+++ scripts/clean.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+rm -rf src/BooksServiceTransaction/__pycache__
+
+rm -rf test/BooksServiceTransactionTests/.pytest_cache
+rm -rf test/BooksServiceTransactionTests/__pycache__
+
+rm -rf src/bin
+rm -rf src/build
+rm -rf src/obj
+rm -rf src/*.egg-info
+
+rm -rf .mypy_cache
