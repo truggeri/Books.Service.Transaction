af80da0f92be93d1d4a7d4700bff096e7286a0f0
Thomas Ruggeri <truggeri@gmail.com>
2018-06-04 01:52:08 -0400

    Updating deploy script.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-pip install --requirement .requirements.txt
-cd src/BooksServiceTransaction && uwsgi --http 0.0.0.0:5000 --module BooksServiceTransaction:app --enable-threads
+pip install --requirement ./requirements.txt
+uwsgi --http 0.0.0.0:5000 --module transaction:app --enable-threads
