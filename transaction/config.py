73a3cc21e1b79ac14f95b6abbaa761ad631fbd93
Thomas Ruggeri <truggeri@gmail.com>
2018-06-09 13:37:11 -0400

    More pythonic check of empty configs.

--- transaction/config.py
+++ transaction/config.py
@@ -7,6 +7,6 @@ with open(CONFIG_FILE) as json_data_file:
     cfg = json.load(json_data_file)
 
 for key in ["DB_USERNAME", "DB_PASSWORD", "DB_HOST_URL"]:
-    if not key in cfg:
+    if key not in cfg:
         cfg[key] = None
     cfg[key] = os.environ.get(key, cfg[key])
