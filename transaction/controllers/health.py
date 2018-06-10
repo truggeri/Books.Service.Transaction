7c2978d7d9b427afed0d3a40b9cdc3e119124bcc
Thomas Ruggeri <truggeri@gmail.com>
2018-06-09 20:13:10 -0400

    Adding db health check to controller.

--- transaction/controllers/health.py
+++ transaction/controllers/health.py
@@ -3,9 +3,11 @@
 """
 
 import json
-from typing import Tuple
 
 from flask_classful import FlaskView, route
+from typing import Tuple
+
+from transaction.storage.database import Database
 
 class HealthController(FlaskView):
     """A class of health related endpoints"""
@@ -15,6 +17,16 @@ class HealthController(FlaskView):
         return ("ok", 200)
 
     @route("health", methods=["GET"])
-    def health(self):
+    def health(self) -> Tuple[str, int]:
         result = {"Healthy" : True}
         return (json.dumps(result, indent=4), 200)
+
+    @route("health/db", methods=["GET"])
+    def db_health(self) -> Tuple[str, int]:
+        client = Database()
+        client.connect()
+        (result, info) = client.health()
+        client.disconnect()
+        if not result:
+            return (json.dumps({"Healthy": False}), 502)
+        return (json.dumps(info, indent=4), 200)
